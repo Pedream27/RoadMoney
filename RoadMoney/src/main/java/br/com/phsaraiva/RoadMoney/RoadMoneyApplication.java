c706f5f7ae13a79d923dcad09804671dedb60344
Pedro Saraiva <pedreamsaraiva@gmail.com>
2025-02-28 12:37:53 -0300

    Feat: Criando back-end

---
+++ RoadMoney/src/main/java/br/com/phsaraiva/RoadMoney/RoadMoneyApplication.java
@@ -0,0 +1,13 @@
+package br.com.phsaraiva.RoadMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadMoneyApplication.class, args);
+	}
+
+}
---
+++ RoadMoney/src/test/java/br/com/phsaraiva/RoadMoney/RoadMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phsaraiva.RoadMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
